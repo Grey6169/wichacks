f865f6ce8e01e0542e4b2e1b95a254584fc585e5
lo6169 <lo6169@rit.edu>
2022-02-26 11:54:50 -0500

    Dropdown menu parts added

--- bravepet/src/components/navbar.js
+++ bravepet/src/components/navbar.js
@@ -9,8 +9,26 @@ function NavBar() {
         
         <span className='bar'>
             <Link to="/home" className='linkItem'>Home</Link>
-            <Link to="/" className='linkItem'>Foster Information</Link>
-            <Link to="/" className='linkItem'>Support</Link>
+            <span className='dropdown'>
+                <Link to="/" className='linkItem'>Foster Information</Link>
+                <div className='dropdown-content'>
+                    <Link to="/" className='dropdownItem'>Become a foster </Link> 
+                    <Link to="/" className='dropdownItem'>Sign in </Link>
+                    <Link to="/" className='dropdownItem'>Find fosters </Link>
+                    <Link to="/" className='dropdownItem'>Find shelters </Link>
+                </div>
+            </span>
+
+            <span className='dropdown'>
+                <Link to="/" className='linkItem'>Support</Link>
+                <div className='dropdown-content'>
+                    <Link to="/" className='dropdownItem'>Find shelters </Link> 
+                    <Link to="/" className='dropdownItem'>Find counselors </Link>
+                    <Link to="/" className='dropdownItem'>Helpful websites </Link>
+                    <Link to="/" className='dropdownItem'>National Groups </Link>
+                </div>
+            </span>
+            
             <Link to="/" className='linkItem'>Donate</Link>
         </span>
         
